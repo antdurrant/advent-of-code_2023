e095e4503276ca2a91d34c8f772117e4857ec7ef
Anthony Durrant <52227638+antdurrant@users.noreply.github.com>
2023-12-11 09:09:40 +0900

    Update day_2.R

--- R/day_2.R
+++ R/day_2.R
@@ -4,32 +4,24 @@
 
 # get the highest number for each of the colours for each of the games
 
-# part 1 ----
-# how many of the games are possible with red<=12,green<=13,blue<=14?
-
 
 extract_pattern <- function(x, pattern){
-  m <- gregexpr(pattern, x)
-  regmatches(x, m)
+  regmatches(x, gregexpr(pattern, x))
 }
 
 get_max_per_colour <- function(x, colour){
   extract_pattern(x, paste0("\\d+ ", colour)) |>
-    Map(f = \(x) extract_pattern(x, "\\d+")) |>
-    Map(f = \(x) as.integer(unlist(x)))|>
-    sapply(max)
+    sapply(\(x) max(as.integer(unlist(extract_pattern(x, "\\d+"))))) 
 }
 
 get_max_per_game <- function(x){
-  data.frame(
-    blue = get_max_per_colour(x, "blue"),
-    green = get_max_per_colour(x, "green"),
-    red = get_max_per_colour(x, "red")
-  )
+  Map(\(y) get_max_per_colour(x, y), c("blue", "green", "red"))
 }
 
-part_1 <- function(x){
-  sum(which(x$red <= 12 & x$green <= 13 & x$blue <= 14))
+# part 1 ----
+# how many of the games are possible with red<=12,green<=13,blue<=14?
+part_1 <- function(x, r = 12, g = 13, b = 14){
+  sum(which(x$red <= r & x$green <= g & x$blue <= b))
 }
 
 # part 2 ----
@@ -38,7 +30,7 @@ part_2 <- function(x){
   sum(x$red * x$blue * x$green)
 }
 
-games <- get_max_per_game(day_2)
+# go go go
+games <- get_max_per_game(readLines(file.path("data", "day_2", "input")))
 part_1(games)
 part_2(games)
-
